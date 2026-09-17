51addf404e07bd46d695c999b723366a4b12f981
Meet Dave <meetdave33@gmail.com>
2026-09-17 17:07:10 -0600

    Remove EC2 public IP

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" {
 
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [aws_security_group.web.id]
-  associate_public_ip_address = true
+  associate_public_ip_address = false
 
   iam_instance_profile = aws_iam_instance_profile.ec2_ssm.name
 
