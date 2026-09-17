8eaa1c492605d34f00aa980336e21672eed1666c
Meet Dave <meetdave33@gmail.com>
2026-09-17 17:57:21 -0600

    Add EC2 user data bootstrap

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,29 @@ resource "aws_instance" "web" {
   vpc_security_group_ids      = [aws_security_group.web.id]
   associate_public_ip_address = true
 
+  user_data = <<-EOF
+  #!/bin/bash
+
+  dnf install -y httpd
+
+  cat <<'HTML' > /var/www/html/index.html
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>AWS CI/CD Demo</title>
+  </head>
+  <body>
+    <h1>Hello from AWS EC2!</h1>
+    <p>Apache was configured automatically using Terraform user_data.</p>
+  </body>
+  </html>
+  HTML
+
+  systemctl enable httpd
+  systemctl start httpd
+  EOF
+
+
   iam_instance_profile = aws_iam_instance_profile.ec2_ssm.name
 
   tags = {
